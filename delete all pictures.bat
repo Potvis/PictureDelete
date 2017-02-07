af9251eac8493c970abdd54cf9a189faccc5e0f7
Junior Vandamme <junior.vandamme1997@gmail.com>
2017-02-07 11:34:10 +0100

    Add files via upload

---
+++ delete all pictures.bat
@@ -0,0 +1,25 @@
+echo off
+set "var=%cd%"
+echo ##################################################################
+echo #                                                                #
+echo # This script will delete ALL jpg's and png's from this folder:  #
+echo #                     	%cd%                         #
+echo #                                                                #
+echo #               Are you sure you want to do this?                #
+echo #                                                                #
+echo ##################################################################
+PAUSE
+echo #################################################################
+echo #                                                               #
+echo #          You won't be able to restore your pictures!          #
+echo #                                                               #
+echo #################################################################
+PAUSE
+DEL /s C:\workspace\crops\*.png
+DEL /s C:\workspace\crops\*.jpg
+echo #################################################################
+echo #                                                               #
+echo #                         JOB DONE                              #
+echo #                                                               #
+echo #################################################################
+PAUSE
